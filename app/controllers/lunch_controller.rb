b53691a7af930f7212b7d052a08a01aef43fcaff
Kyosuke Kobayashi <kyosukekobayashi@KyosukenoMacBook-Pro.local>
2019-05-10 16:04:15 +0900

    スペルミス修正

--- app/controllers/lunch_controller.rb
+++ app/controllers/lunch_controller.rb
@@ -1,6 +1,6 @@
 class LunchController < ApplicationController
     def go
-        eatarites = ['龍郎','志恩','むじゃき','銀の豚']
-        @recommend = eatarites.sample
+        eataries = ['龍郎','志恩','むじゃき','銀の豚']
+        @recommend = eataries.sample
     end
 end
